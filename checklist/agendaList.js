265db866c612a19aa82bbf467b7c71b19e52cc04
ThandiBrown <thanbrown24@gmail.com>
2024-02-09 04:18:08 -0600

    updated agenda

--- checklist/agendaList.js
+++ checklist/agendaList.js
@@ -1,12 +1,32 @@
 
 function returnAgenda() {
     return [
-        'Brush Teeth',
-        'Wash Face',
+        'Morning Hygiene',
         'Morning Water',
         'Morning Body Warm Up',
         'Hot Tea',
         'Hot Broth',
+        
+        'Work out and eat healthy',
+        
+        'Night routine',
+        'Wash dishes',
+        'Prep for next day: meals, clothes, room, schedule, tasks to complete and when',
+        'Clean up room',
+        
+        'Wrap hair',
+        'Evening Hygiene',
+        'Skin routine',
+        'Fresh water',
+        
+        'Night stretching',
+        'Appropriate attire to prevent overheating',
+        
+        'Turn off dragon, talon & computer',
+        'Turn off mouse and keyboard',
+        'Eye glass case',
+        'Hand braces',
+        'Eye mask',
     ];
 }
 
