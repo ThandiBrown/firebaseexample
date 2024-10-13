a181e9e54e6a69dee238298b5afc2065682075cc
ThandiBrown <thanbrown24@gmail.com>
2024-10-13 14:24:59 -0500

    return returns

--- overall/comms/foundation.js
+++ overall/comms/foundation.js
@@ -50,10 +50,10 @@ function initializeFirebase() {
 							const provider = new GoogleAuthProvider();
 							if (isMobile()) {
 								console.log('You are on a mobile device');
-								signInWithRedirect(auth, provider);
+								return signInWithRedirect(auth, provider);
 							} else {
 								console.log('You are on a desktop or non-mobile device');
-								signInWithPopup(auth, provider);
+								return signInWithPopup(auth, provider);
 							}
 
 						})
